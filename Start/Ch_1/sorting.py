a87abc3ac8b84ea5f02b25ea83a1f8f08ffc9118
jenniferfjy <114460493+jenniferfjy@users.noreply.github.com>
2023-08-03 08:47:01 +0000

    Finish sorting earthquake mag

--- Start/Ch_1/sorting.py
+++ Start/Ch_1/sorting.py
@@ -8,21 +8,27 @@ numbers = [42, 54, 19, 17, 23, 31, 16, 4]
 names = ["Jeff", "Bill", "Addie", "Stephanie", "Zach", "Lukas", "Joe", "Stacy"]
 
 # TODO: the sorted() function can be used to return a new list with sorted data
-
+newlist = sorted(numbers)
+print(newlist)
 
 # TODO: alternately, you can use the list object's sort() method, which sorts the list in-place
-
+numbers.sort(reverse=True)
+print(numbers)
 
 # TODO: To sort custom objects, we can tell the sort function which property to use
 # by specifying a key function
-
 # open the data file and load the JSON
-# with open("../../30DayQuakes.json", "r") as datafile:
-#     data = json.load(datafile)
+with open("30DayQuakes.json", "r") as datafile:
+    data = json.load(datafile)
 
 
-# def getmag(dataitem):
-#     magnitude = dataitem["properties"]["mag"]
-#     if (magnitude is None):
-#         magnitude = 0
-#     return float(magnitude)
+def getmag(dataitem):
+    magnitude = dataitem["properties"]["mag"]
+    if (magnitude is None):
+        magnitude = 0
+    return float(magnitude)
+
+
+data["features"].sort(key=getmag, reverse=True)
+for i in range(10):
+    print(data["features"][i]["properties"]["place"])
